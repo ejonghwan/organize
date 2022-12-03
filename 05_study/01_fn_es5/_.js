fb8f08bf7954cadc94b5e9237ab4863232025268
ejonghwan <sun87-1@daum.net>
2022-12-03 14:02:37 +0900

    asd

--- 05_study/01_fn_es5/_.js
+++ 05_study/01_fn_es5/_.js
@@ -1,4 +1,26 @@
 
+/*
+    함수형 프로그래밍
+    1. 순수 함수들의 평가시점들을 다루거나 
+    2. 함수가 함수를 리턴하거나 
+    3. 함수가 함수를 실행하거나 
+    4. 함수를 값으로 사용하거나 
+    5. 인자로 받은 함수의 값을 반전시키거나 
+    6. 인자로 받은 함수의 값을 사용해서 또 다른 함수의 값을 내보내거나
+
+    이런식으로 함수의 조합과 응용을 강조하는게 함수형 프로그래밍
+
+    *외부에 영향을 주지도 받지도 않는 한가지 기능을 하는 간결한 함수들을 서로 조합하는게 중요.
+    *10개의 복잡하고 긴 함수를 만드는 것보다 100개의 다양한 함수를 만드는게 프로그래밍에서는 유리함
+
+
+    고차함수 모두 같냐(_every) 한개만 같냐(_some)을 선택하고 보조함수로 
+    상세 조건을 선택하게 하여 조립하는? 조합하는 프로그래밍
+
+*/
+
+
+
 var users = [
     { id:1, name: "ID", age: 36 },
     { id:2, name: "BJ", age: 12 },
@@ -11,12 +33,9 @@ var users = [
 ]
 
 
-let _get2 = _curryr(function(obj, key) {
-    return obj == null ? undefined : obj[key]
-});
 
-let curryr_map = _curryr(_map)
-let curryr_filter = _curryr(_filter)
+let c_map = _curryr(_map)
+let c_filter = _curryr(_filter)
 
 
 
@@ -54,11 +73,15 @@ function _map(iter, fn) {
     return new_list;
 }
 
-
+// key를 입력하면 return value 
 function _get(obj, key) {
     return obj == null ? undefined : obj[key]
 };
 
+// key를 입력하면 return value. 커링으로도 가능
+let _get2 = _curryr(function(obj, key) {
+    return obj == null ? undefined : obj[key]
+});
 
 function _curry(fn) {
     return function(a, b) {
@@ -124,4 +147,70 @@ function _values(data) {
 
 function _identity(val) {
     return val;
+}
+
+
+function _pluck(data, key) {
+    return curryr_map(data, _get2(key))
+}
+
+// 함수를 받아 반대만 리턴하는 함수 
+function _negate(func) {
+    return function(val) {
+        return !func(val)
+    }
+}
+
+// 조건의 반대를 찾음
+function _reject(data, fn) {
+    return _filter(data, _negate(fn))
+}
+
+// 필터함수의 null false 0 등을 제외한 값만 가져옴
+let compact = c_filter(_identity)
+
+
+// 조건에 맞는 한개만 찾음 
+function _find(list, fn) {
+    let keys = _keys(list);
+    for(let i = 0; i < keys.length; i++) {
+        let val = list[keys[i]];
+        if(fn(val)) return val
+    }
+}
+
+// 조건에 맞는 한개의 인덱스만 찾음
+function _find_index(list, fn) {
+    let keys = _keys(list);
+    for(let i = 0; i < keys.length; i++) {
+        let val = list[keys[i]];
+        if(fn(val)) return i
+    }
+}
+
+let c_find = _curryr(function(list, fn) {
+    let keys = _keys(list);
+    for(let i = 0; i < keys.length; i++) {
+        let val = list[keys[i]];
+        if(fn(val)) return val
+    }
+})
+
+
+let c_find_index = _curryr(function(list, fn) {
+    let keys = _keys(list);
+    for(let i = 0; i < keys.length; i++) {
+        let val = list[keys[i]];
+        if(fn(val)) return i
+    }
+    return -1;
+})
+
+function _some(data, fn) {
+    // fn이 만족하는인덱스를 찾아보고 찾아지느게 있으면 트루
+    return c_find_index(data, fn || _identity) != -1;
+}
+
+function _every(data, fn) {
+    return c_find_index(data, _negate(fn || _identity)) == -1;
 }
